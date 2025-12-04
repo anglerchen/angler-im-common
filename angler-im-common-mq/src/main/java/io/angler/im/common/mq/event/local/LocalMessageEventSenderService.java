a23bda68863b7de11903a6f70a5f5d5e3ff7e617
hc <911360915@qq.com>
2025-12-04 14:28:16 +0800

    init

---
+++ angler-im-common-mq/src/main/java/io/angler/im/common/mq/event/local/LocalMessageEventSenderService.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.angler.im.common.mq.event.local;
+
+import com.alibaba.cola.event.EventBusI;
+import io.angler.im.common.domain.model.TopicMessage;
+import io.angler.im.common.mq.event.MessageEventSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "message.mq.event.type", havingValue = "cola")
+public class LocalMessageEventSenderService implements MessageEventSenderService {
+    @Autowired
+    private EventBusI eventBus;
+
+    @Override
+    public boolean send(TopicMessage message) {
+        eventBus.fire(message);
+        return true;
+    }
+}
